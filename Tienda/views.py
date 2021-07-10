b9d1810b9ce8d989142b571540fab1fc20c1795a
Christian <kriz10@outlook.com>
2021-07-09 18:19:59 -0600

    URLs

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render (request, 'dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = Customer.objects.all().count()
+    total_orders = Order.objects.all().count()
+    delivered = Order.objects.all().filter(status = 'Delivered').count()
+    pending = Order.objects.all().filter(status = 'Pending').count()
+    context = {
+        'orders' : orders,
+        'customers' : customers,
+        'total_customers' : total_customers,
+        'total_orders' : total_orders,
+        'delivered' : delivered,
+        'pending' : pending
+    }
+    return render (request, 'dashboard.html', context)
 
 def products(request):
-    return render (request, 'products.html') 
+    products = Product.objects.all()
+    return render (request, 'products.html', {'products':products}) 
 
-def customer(request):
-    return render (request, 'customer.html')
+def customer(request,pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {
+        'customer' : customer,
+        'orders' : orders,
+        'order_count' : order_count
+    }
+    return render (request, 'customer.html',context)
